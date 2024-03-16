becce71fc5dad3d3dd12305d5be823f615bd76d2
Joshua Barlow <jalapenojosh@gmail.com>
2024-03-15 23:02:04 -0600

    I believe this works.

--- RaftTests/UnitTest1.cs
+++ RaftTests/UnitTest1.cs
@@ -63,17 +63,17 @@ namespace RaftTests
 
         }
 
-        [Test]
-        public async Task Test1CallToAnotherWorks()
-        {
-            string shouldBe1 = await gateway.ReturnIdFromSecondNode("2", "1");
-            string shouldBe2 = await gateway.ReturnIdFromSecondNode("3", "2");
-            string shouldBe3 = await gateway.ReturnIdFromSecondNode("1", "3");
-            Console.WriteLine(shouldBe1);
-            Console.WriteLine(shouldBe2);
-            Console.WriteLine(shouldBe3);
-            Test3AtOnce(shouldBe1, shouldBe2, shouldBe3);
-        }
+        // [Test]
+        // public async Task Test1CallToAnotherWorks()
+        // {
+        //     string shouldBe1 = await gateway.ReturnIdFromSecondNode("2", "1");
+        //     string shouldBe2 = await gateway.ReturnIdFromSecondNode("3", "2");
+        //     string shouldBe3 = await gateway.ReturnIdFromSecondNode("1", "3");
+        //     Console.WriteLine(shouldBe1);
+        //     Console.WriteLine(shouldBe2);
+        //     Console.WriteLine(shouldBe3);
+        //     Test3AtOnce(shouldBe1, shouldBe2, shouldBe3);
+        // }
 
         // [Test]
         // public void TestReturnList()
